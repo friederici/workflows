a6e70c6fa64158ef8513b5b757bfa2f33ca509aa
Florian Friederici <friederici@gmx.de>
2023-10-28 10:45:24 +0200

    randomize created files

--- data/createData.py
+++ data/createData.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-for num in range(1,11):
+
+from random import randrange
+
+for num in range(1,21):
 	fp = open(f'{num}.txt', 'w')
-	fp.write('0'*500)
+	fp.write('0'*randrange(500,4000))
 	fp.close()
