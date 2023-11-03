21b57e0c02ff2e59b7f694a5dd9c76c168adad6c
Florian Friederici <44371773+friederici@users.noreply.github.com>
2023-11-03 14:16:22 +0100

    save all traces at once

--- save-traces.sh
+++ save-traces.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-echo "save traces"
-date
-
-for dir in synt*
-do
-	cd $dir
-	pwd
-
-	tar czf ../$dir.tgz TaskScaler_*
-
-	cd ..
-done
-
-echo "finished saving traces"
-date
+zip result.zip synt?/TaskScaler*
 
