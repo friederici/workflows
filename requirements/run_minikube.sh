f2fbc819b96c8b2922f2326c72f0c89933a7e224
Florian Friederici <44371773+friederici@users.noreply.github.com>
2023-11-05 10:11:01 +0100

    increase resources

--- requirements/run_minikube.sh
+++ requirements/run_minikube.sh
@@ -6,7 +6,7 @@ else
   nodes=$1
 fi
 
-minikube start --nodes $nodes --feature-gates=InPlacePodVerticalScaling=true
+minikube start --nodes $nodes --feature-gates=InPlacePodVerticalScaling=true --memory 6144 --cpus 3
 
 minikube kubectl -- apply -f k8s.yaml
 minikube kubectl -- apply -f volume-minikube.yaml
