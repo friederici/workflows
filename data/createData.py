ffedc12e7eab9f5b0d7d5195acbe084921fb0119
Florian Friederici <44371773+friederici@users.noreply.github.com>
2023-11-05 07:44:07 +0100

    changed data

--- data/createData.py
+++ data/createData.py
@@ -3,12 +3,12 @@
 from random import randrange
 
 for num in range(1,8):
-	fp = open(f'{num}.txt', 'w')
+	fp = open(f'txt.{num}.txt', 'w')
 	fp.write('0'*randrange(300,3000))
 	fp.close()
 
 for num in range(1,8):
-	fp = open(f'{num}.wf3', 'w')
-	fp.write('3'*num*10)
+	fp = open(f'wf3.{num}.wf3', 'w')
+	fp.write('3'*randrange(5,70))
 	fp.close()
 
