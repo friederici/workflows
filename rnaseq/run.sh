067380c73275e549d053fb0df11cf6ab19c7fc8e
Florian Friederici <44371773+friederici@users.noreply.github.com>
2023-11-08 07:35:15 +0100

    delete old files before run

--- rnaseq/run.sh
+++ rnaseq/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd /nfs/data/output
+rm -rf *
+
 cd /nfs/input/pipelines/rnaseq
-nextflow run main.nf -config /workflows/config/rnaseq.config -config /workflows/config/linear.config --outdir /nfs/data/output
+rm -rf work
+rm -rf .nextflow*
+rm Task*
+
+nextflow run main.nf -config /workflows/config/rnaseq.config -config /workflows/config/wary.config --outdir /nfs/data/output
 
