d9cb903396ff0fe735e054657a6b78c418c707f4
Florian Friederici <44371773+friederici@users.noreply.github.com>
2023-11-10 20:35:04 +0100

    script for rnaseq run

--- rnaseq/run.sh
+++ rnaseq/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-cd /nfs/data/output
-rm -rf *
+for i in $(seq 1 10);
+do
+	echo $i
+	cd /nfs/data/output
+	rm -rf *
 
-cd /nfs/input/pipelines/rnaseq
-rm -rf work
-rm -rf .nextflow*
-rm Task*
+	cd /nfs/input/pipelines/rnaseq
+	rm -rf work
+	rm -rf .nextflow*
+	rm Task*
 
-nextflow run main.nf -config /workflows/config/rnaseq.config -config /workflows/config/wary.config --outdir /nfs/data/output
+	nextflow run main.nf -config /workflows/config/rnaseq.config -config /workflows/config/wary.config --outdir /nfs/data/output
 
+	zip /workflows/rnaseq/result_rnaseq_$i.zip Task* trace* .nextflow.log*
+done
