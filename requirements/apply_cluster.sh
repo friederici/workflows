8436fdf93a15eebe84cdc1a9e6bf613493384de8
Florian Friederici <44371773+friederici@users.noreply.github.com>
2023-11-25 17:14:42 +0100

    change /nfs to be hostpath

--- requirements/apply_cluster.sh
+++ requirements/apply_cluster.sh
@@ -2,7 +2,7 @@
 
 kubectl apply -f /workflows/requirements/k8s.yaml
 kubectl apply -f /workflows/requirements/volume-hostpath.yaml
-kubectl apply -f /workflows/requirements/volume-nfs.yaml
+kubectl apply -f /workflows/requirements/volume-nfs-hostpath.yaml
 kubectl apply -f /workflows/requirements/cws-hostpath.yaml
 
 kubectl wait --for=condition=ready pod workflow-scheduler --namespace workflows
--- requirements/clear_cluster.sh
+++ requirements/clear_cluster.sh
@@ -2,6 +2,7 @@
 
 kubectl delete namespace workflows
 kubectl delete pv nfs-pv
+kubectl delete pv nfs-hostpath-pv
 kubectl delete pv workflows-pv
 kubectl delete pv workflows-hostpath-pv
 
